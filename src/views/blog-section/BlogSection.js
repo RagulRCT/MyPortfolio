4545f4479d4c4b9f5a8509f0aad191e3181ca32e
RagulRCT <107754208+RagulRCT@users.noreply.github.com>
2022-08-11 10:43:47 +0530

    Initial commit

---
+++ src/views/blog-section/BlogSection.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import Tilt from "react-parallax-tilt";
+import "./BlogSection.css";
+
+const BlogSection = () => {
+  return (
+    <div id="blogs">
+      <hr></hr>
+    </div>
+  );
+};
+export default BlogSection;
---
+++ src/views/projects/projects.js
@@ -0,0 +1,451 @@
+import React from "react";
+import {
+  Timeline,
+  Events,
+  UrlButton,
+  ImageEvent,
+  createTheme,
+  themes
+} from "@merc/react-timeline";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+//Css file
+import "./project.css";
+
+//Projects
+import ecomm from "../../assets/ecomm.PNG";
+import inv from "../../assets/inv.png";
+import rma from "../../assets/rma.PNG";
+import ChatKharaMeal from "../../assets/img/projects/ChatKharaMeal-Website.png";
+import ExpenseTracker from "../../assets/img/projects/Expense-TrackerApp.png";
+import MusicList from "../../assets/img/projects/Music-List-App.png";
+import QuizApp from "../../assets/img/projects/QuizzApp-JavaScript.png";
+
+//Skills
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
+import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+import L_FIREBASE from "../../assets/img/skills/firebase-icon.svg";
+
+// Custom timline theme
+const customTheme = createTheme(themes.default, {
+  card: {
+    backgroundColor: "#efefef"
+  },
+  date: {
+    backgroundColor: "#ec4b4f"
+  },
+  marker: {
+    borderColor: "#070d59"
+  },
+  timelineTrack: {
+    backgroundColor: "#ec4b4f"
+  }
+});
+
+const ProjectTimeline = () => {
+  return (
+    <div id="projects">
+      <h1 className="pt-3 text-center font-details-b pb-3">PROJECTS</h1>
+      <Timeline theme={customTheme}>
+        <Events>
+          {/* //ChatKharaMeal */}
+          <ImageEvent
+            date="February, 2022"
+            className="text-center"
+            text="Mobile Ecommerce"
+            src={ecomm}
+            alt="ecomm"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This is an E-Commerce
+                        website of Mobile Store, which provides user online
+                        Mobile shopping experience with various varieties.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>
+                            Provide best quality of Mobiles in low price rate.
+                          </li>
+                          <li>Powered by Pure JavaScript, React Js and CSS</li>
+                          <li>Respoisive Design</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_JAVASCRIPT}
+                                alt="JavaScript"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              JavaScript
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React Js
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://ecommerse-delta.vercel.app/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* //ExpenseTracker */}
+
+          <ImageEvent
+            date="April, 2022"
+            className="text-center"
+            text="Modular Agency Website"
+            src={rma}
+            alt="rma"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This is a Modular Agency
+                        app done for my dad's company. It is User friendly and
+                        easy to use.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>
+                            Keep your Clients up to date you about the Agency /
+                            Company
+                          </li>
+                          <li>
+                            Powered by React Js, JavaScript, HTML and CSS.
+                          </li>
+                          <li>Used React Router for Routing pages</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_JAVASCRIPT}
+                                alt="JavaScript"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              JavaScript
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React Js
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="hhttps://reliance-rma.vercel.app/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* QuizApp */}
+
+          <ImageEvent
+            date="June, 2022"
+            className="text-center"
+            text="Quiz App"
+            src={QuizApp}
+            alt="Quizz App"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> Basic Quiz app, to test
+                        your computer knowledge. Also, The question and its
+                        options changed in every page reload.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Test your basic computer knowledge </li>
+                          <li>API used for computer science quiz</li>
+                          <li>Powered by JavaScript, HTML and CSS.</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_JAVASCRIPT}
+                                alt="JavaScript"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              JavaScript
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_BOOTSTRAP}
+                                alt="bootstrap"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Bootstrap
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://my-quiz-app-xi.vercel.app/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+
+          {/* //MusicListApp */}
+
+          <ImageEvent
+            date="July, 2022"
+            className="text-center"
+            text="Invoice Generator"
+            src={inv}
+            alt="invoice"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> Invoice Generator For
+                        Official Companies.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>
+                            {" "}
+                            An Invoice creator project built with React.{" "}
+                          </li>
+                          <li>Attractive UI, Easy to use.</li>
+                          <li>
+                            Uses jspdf-react to capture the data from the modal
+                            and convert it from canvas -> pdf
+                          </li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React Js
+                            </span>
+                          </li>
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://my-invoice-generator.vercel.app/"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+              </div>
+            </div>
+          </ImageEvent>
+        </Events>
+      </Timeline>
+    </div>
+  );
+};
+
+export default ProjectTimeline;
